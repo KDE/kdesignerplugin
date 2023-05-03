6f4edf4212a8b14d77697c958323d0498cb3a701
Christoph Cullmann <cullmann@kde.org>
2023-05-03 19:49:59 +0200

    Removed for KF6, the 'kf5' branch contains the last maintained state.

--- KF5DesignerPluginConfig.cmake
+++
@@ -1,3 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/KF5DesignerPluginTargets.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/KF5DesignerPluginMacros.cmake)
-set(KF5DESIGNERPLUGIN_GENERATOR_EXECUTABLE KF5::kgendesignerplugin)
